3cff634893ce8125cdfe92392a448815ed9cb85d
Farhan Ghazali <fghazali@bousteadgeospatial.com>
2019-08-30 11:30:47 +0800

    Hot rel

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const svelteLoaders = [{
       }
     },
     emitCss: true,
-    hotReload: false, // https://github.com/sveltejs/svelte/issues/2377
+    hotReload: false, // https://github.com/sveltejs/svelte/issues/2377 || https://github.com/sveltejs/svelte/pull/3148
     hotOptions: {
       // will display compile error in the client, avoiding page reload on error
       optimistic: false
